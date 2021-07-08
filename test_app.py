ca7a2e91cdadd3a68259dd9c93e376f1b71ff2d4
suryabharadwaj143 <suryabharadwaj097@gmail.com>
2021-07-08 15:25:08 +0530

    Written Two Test Cases

--- test_app.py
+++ test_app.py
@@ -24,3 +24,20 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+#Task 2 Writing Test Cases
+# test to check the correct functioning of the /ping route
+def test_iris():
+    with TestClient(app) as client:
+        response = client.get("/iris")
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"iris": "This is a iris flower prediction website"}
+
+# test to check the correct functioning of the /ping route
+def test_prediction():
+    with TestClient(app) as client:
+        response = client.get("/prediction")
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"prediction": "We predicted our test cases"}
